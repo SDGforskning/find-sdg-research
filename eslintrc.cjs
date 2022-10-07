86aba8edb9841919dc2df8071a95d9bbd1cd0e91
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2022-10-07 07:20:25 +0200

    remove escape in code block

---
+++ eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  reportUnusedDisableDirectives: true,
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'latest'
+  },
+}
