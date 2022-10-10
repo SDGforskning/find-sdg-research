a6a412a6e1ba5cf15aafbf2c1e4552d6e638c0dd
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2022-10-10 16:26:18 +0200

    prototype result view

--- components/search/SDGResultView.tsx
+++ components/search/SDGResultView.tsx
@@ -1,3 +1,9 @@
+const randomGoals = [
+  'sdg1',
+  'sdg3',
+  'sdg14',
+]
+
 const CustomResultView = ({
   result,
   onClickLink
@@ -5,49 +11,36 @@ const CustomResultView = ({
   result;
   onClickLink: () => void;
 }) => (
-  <li className="sui-result">
-    <div className="sui-result__header">
-      <h3>
+  <li className='border rounded border-slate-700 mt-5 p-5'>
+    <div>
+      <h3 className='text-3xl font-bold'>
         {/* Maintain onClickLink to correct track click throughs for analytics*/}
         <a onClick={onClickLink} href={result.nps_link.raw}>
           {result.title.raw}
         </a>
       </h3>
-    </div>
-    <div className="sui-result__body">
-      {/* use 'raw' values of fields to access values without snippets */}
-      <div className="sui-result__image">
-        <img src={result.image_url.raw} alt="" />
-      </div>
-
-      {/* Use the 'snippet' property of fields with dangerouslySetInnerHtml to render snippets */}
       <div
-        className="sui-result__details"
-        dangerouslySetInnerHTML={{ __html: result.description.raw }}
-      ></div>
-
-      {/* <pre>{JSON.stringify(result, null, 2)}</pre> */}
-    </div>
-    {result.states?.raw?.map(state => (
-      <div
-        className="ml-4 text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-green-200 text-green-700 rounded-full"
+        className={`ml-4 text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-green-500 dark:text-white rounded-full`}
       >
-        {/* <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          className="feather feather-arrow-right mr-2"
-        >
-          <line x1="5" y1="12" x2="19" y2="12"></line>
-          <polyline points="12 5 19 12 12 19"></polyline>
-        </svg> */}
-        {state}
+        Journal
+      </div>
+      <div
+        className={`ml-4 text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-green-500 dark:text-white rounded-full`}
+      >
+        Scientific
+      </div>
+    </div>
+
+    <ul>
+      <li><a href='https://cristin.no'>Se forfattere og inst. i CRISTIN</a></li>
+      <li>doi: <a href='https://doi.org/10.3352/jeehp.2013.10.3'>doi.org/10.3352/jeehp.2013.10.3</a></li>
+      <li>open link: <a href='https://example.org'>link to?</a></li>
+    </ul>
+    {randomGoals.map(goal => (
+      <div
+        className={`ml-4 text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-${goal} dark:text-white rounded-full`}
+      >
+        {goal}
       </div>
     ))}
   </li>
