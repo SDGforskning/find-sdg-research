3dcc872ac1e746fb48ad58527cf8366685fecb4c
Caroline Armitage <42732381+Caroline-A@users.noreply.github.com>
2022-10-26 11:28:04 +0200

    Updated target mappings EN

--- lib/sdgMappings.ts
+++ lib/sdgMappings.ts
@@ -190,7 +190,7 @@ export const sdgTopicTargetLabelMapping = {
     no: "",
   },
   SDG02_0a: {
-    en: "2.a Investment in developing countries",
+    en: "2.a Investment (developing countries)",
     no: "",
   },
   SDG02_b: {
@@ -242,22 +242,546 @@ export const sdgTopicTargetLabelMapping = {
     no: "",
   },
   SDG03_b: {
-    en: "3.b Access to medicines and vaccines",
+    en: "3.b Access to medicines/vaccines",
     no: "",
   },
   SDG03_c: {
-    en: "3.c Recruitment in developing countries",
+    en: "3.c Recruitment (developing countries)",
     no: "",
   },
   SDG03_d: {
     en: "3.d Risk preparedness",
     no: "",
   },
+  SDG04_01: {
+    en: "4.1 Primary education",
+    no: "",
+  },
+  SDG04_02: {
+    en: "4.2 Early childhood",
+    no: "",
+  },
+  SDG04_03: {
+    en: "4.3 Tertiary education",
+    no: "",
+  },
+  SDG04_04: {
+    en: "4.4 Skills",
+    no: "",
+  },
+  SDG04_05: {
+    en: "4.5 Equality",
+    no: "",
+  },
+  SDG04_06: {
+    en: "4.6 Literacy and numeracy",
+    no: "",
+  },
+  SDG04_07: {
+    en: "4.7 Education for sustainability",
+    no: "",
+  },
+  SDG04_0a: {
+    en: "4.a Educational facilities",
+    no: "",
+  },
+  SDG04_b: {
+    en: "4.b Scholarships (developing countries)",
+    no: "",
+  },
+  SDG04_c: {
+    en: "4.c Teachers (developing countries)",
+    no: "",
+  },
+  SDG07_01: {
+    en: "7.1 Clean and modern energy",
+    no: "",
+  },
+  SDG07_02: {
+    en: "7.2 Renewable energy share",
+    no: "",
+  },
+  SDG07_03: {
+    en: "7.3 Energy efficiency",
+    no: "",
+  },
+  SDG07_0a: {
+    en: "7.a Sharing and investment",
+    no: "",
+  },
+  SDG07_b: {
+    en: "7.b Services (developing countries)",
+    no: "",
+  },
+  SDG11_01: {
+    en: "11.1 Housing",
+    no: "",
+  },
+  SDG11_02: {
+    en: "11.2 Transport systems",
+    no: "",
+  },
+  SDG11_03: {
+    en: "11.3 Urbanisation and planning",
+    no: "",
+  },
+  SDG11_04: {
+    en: "11.4 Cultural and natural heritage",
+    no: "",
+  },
+  SDG11_05: {
+    en: "11.5 Disasters",
+    no: "",
+  },
+  SDG11_06: {
+    en: "11.6 Environmental impact",
+    no: "",
+  },
+  SDG11_07: {
+    en: "11.7 Green and public spaces",
+    no: "",
+  },
+  SDG11_0a: {
+    en: "11.a Development planning",
+    no: "",
+  },
+  SDG11_b: {
+    en: "11.b Disaster plans",
+    no: "",
+  },
+  SDG11_c: {
+    en: "11.c Construction (developing countries)",
+    no: "",
+  },
+  SDG13_01: {
+    en: "13.1 Disasters",
+    no: "",
+  },
+  SDG13_02: {
+    en: "13.2 National plans",
+    no: "",
+  },
+  SDG13_03: {
+    en: "13.3 Capacity",
+    no: "",
+  },
+  SDG13_0a: {
+    en: "13.a Climate funding",
+    no: "",
+  },
+  SDG13_b: {
+    en: "13.b Capacity (developing countries)",
+    no: "",
+  },
+  SDG14_01: {
+    en: "14.1 Pollution",
+    no: "",
+  },
+  SDG14_02: {
+    en: "14.2 Conservation and management",
+    no: "",
+  },
+  SDG14_03: {
+    en: "14.3 Ocean acidification",
+    no: "",
+  },
+  SDG14_04: {
+    en: "14.4 Sustinable fishing",
+    no: "",
+  },
+  SDG14_05: {
+    en: "14.5 Protected areas",
+    no: "",
+  },
+  SDG14_06: {
+    en: "14.6 Fisheries subsidies",
+    no: "",
+  },
+  SDG14_07: {
+    en: "14.7 Benefits (SIDS/LEDC)",
+    no: "",
+  },
+  SDG14_0a: {
+    en: "14.a Infrastructure and benefits",
+    no: "",
+  },
+  SDG14_b: {
+    en: "14.b Small-scale fishers",
+    no: "",
+  },
+  SDG14_c: {
+    en: "14.c Laws and conventions",
+    no: "",
+  },
+  SDG15_01: {
+    en: "15.1 Conservation and use",
+    no: "",
+  },
+  SDG15_02: {
+    en: "15.2 Forests",
+    no: "",
+  },
+  SDG15_03: {
+    en: "15.1 Land degredation",
+    no: "",
+  },
+  SDG15_04: {
+    en: "15.4 Mountains",
+    no: "",
+  },
+  SDG15_05: {
+    en: "15.5 Habitats, diversity and extinction",
+    no: "",
+  },
+  SDG15_06: {
+    en: "15.6 Sharing genetic resources",
+    no: "",
+  },
+  SDG15_07: {
+    en: "15.7 Poaching and demand",
+    no: "",
+  },
+  SDG15_08: {
+    en: "15.8 Invasive species",
+    no: "",
+  },
+  SDG15_09: {
+    en: "15.9 Biodiversity planning",
+    no: "",
+  },
+  SDG15_0a: {
+    en: "15.a Financial resources",
+    no: "",
+  },
+  SDG15_b: {
+    en: "15.b Funding for forest management",
+    no: "",
+  },
+  SDG15_c: {
+    en: "15.c Poaching and livlihoods",
+    no: "",
+  },
 }
 
 export const sdgActionTargetLabelMapping = {
+  SDG01_01: {
+    en: "1.1 Extreme poverty",
+    no: "",
+  },
+  SDG01_02: {
+    en: "1.2 Poverty",
+    no: "",
+  },
+  SDG01_03: {
+    en: "1.3 Social protections",
+    no: "",
+  },
+  SDG01_04: {
+    en: "1.4 Rights and Access",
+    no: "",
+  },
+  SDG01_05: {
+    en: "1.5 Disaster resilience",
+    no: "",
+  },
   SDG01_0a: {
-    en: "",
+    en: "1.a Resources",
+    no: "",
+  },
+  SDG01_b: {
+    en: "1.b Policy",
+    no: "",
+  },
+  SDG02_01: {
+    en: "2.1 Hunger",
+    no: "",
+  },  
+  SDG02_02: {
+    en: "2.2 Malnutrition",
+    no: "",
+  },
+  SDG02_03: {
+    en: "2.3 Small-scale food producers",
+    no: "",
+  },
+  SDG02_04: {
+    en: "2.4 Sustainable food production",
+    no: "",
+  },
+  SDG02_05: {
+    en: "2.5 Genetic resources",
+    no: "",
+  },
+  SDG02_0a: {
+    en: "2.a Investment (developing countries)",
+    no: "",
+  },
+  SDG02_b: {
+    en: "2.b Markets and subsidies",
+    no: "",
+  },
+  SDG02_c: {
+    en: "2.c Stable food markets",
+    no: "",
+  },
+  SDG03_01: {
+    en: "3.1 Maternal mortality",
+    no: "",
+  },
+  SDG03_02: {
+    en: "3.2 Child mortality",
+    no: "",
+  },
+  SDG03_03: {
+    en: "3.3 Communicable diseases",
+    no: "",
+  },
+  SDG03_04: {
+    en: "3.4 Non-infectious diseases",
+    no: "",
+  },
+  SDG03_05: {
+    en: "3.5 Substance abuse",
+    no: "",
+  },
+  SDG03_06: {
+    en: "3.6 Traffic injuries",
+    no: "",
+  },
+  SDG03_07: {
+    en: "3.7 Reproductive healthcare",
+    no: "",
+  },
+  SDG03_08: {
+    en: "3.8 Universal healthcare",
+    no: "",
+  },
+  SDG03_09: {
+    en: "3.9 Pollution and contamination",
+    no: "",
+  },
+  SDG03_0a: {
+    en: "3.a Tobacco",
+    no: "",
+  },
+  SDG03_b: {
+    en: "3.b Access to medicines/vaccines",
+    no: "",
+  },
+  SDG03_c: {
+    en: "3.c Recruitment (developing countries)",
+    no: "",
+  },
+  SDG03_d: {
+    en: "3.d Risk preparedness",
+    no: "",
+  },
+  SDG04_01: {
+    en: "4.1 Primary education",
+    no: "",
+  },
+  SDG04_02: {
+    en: "4.2 Early childhood",
+    no: "",
+  },
+  SDG04_03: {
+    en: "4.3 Tertiary education",
+    no: "",
+  },
+  SDG04_04: {
+    en: "4.4 Skills",
+    no: "",
+  },
+  SDG04_05: {
+    en: "4.5 Equality",
+    no: "",
+  },
+  SDG04_06: {
+    en: "4.6 Literacy and numeracy",
+    no: "",
+  },
+  SDG04_07: {
+    en: "4.7 Education for sustainability",
+    no: "",
+  },
+  SDG04_0a: {
+    en: "4.a Educational facilities",
+    no: "",
+  },
+  SDG04_b: {
+    en: "4.b Scholarships (developing countries)",
+    no: "",
+  },
+  SDG04_c: {
+    en: "4.c Teachers (developing countries)",
+    no: "",
+  },
+  SDG07_01: {
+    en: "7.1 Clean and modern energy",
+    no: "",
+  },
+  SDG07_02: {
+    en: "7.2 Renewable energy share",
+    no: "",
+  },
+  SDG07_03: {
+    en: "7.3 Energy efficiency",
+    no: "",
+  },
+  SDG07_0a: {
+    en: "7.a Sharing and investment",
+    no: "",
+  },
+  SDG07_b: {
+    en: "7.b Services (developing countries)",
+    no: "",
+  },
+  SDG11_01: {
+    en: "11.1 Housing",
+    no: "",
+  },
+  SDG11_02: {
+    en: "11.2 Transport systems",
+    no: "",
+  },
+  SDG11_03: {
+    en: "11.3 Urbanisation and planning",
+    no: "",
+  },
+  SDG11_04: {
+    en: "11.4 Cultural and natural heritage",
+    no: "",
+  },
+  SDG11_05: {
+    en: "11.5 Disasters",
+    no: "",
+  },
+  SDG11_06: {
+    en: "11.6 Environmental impact",
+    no: "",
+  },
+  SDG11_07: {
+    en: "11.7 Green and public spaces",
+    no: "",
+  },
+  SDG11_0a: {
+    en: "11.a Development planning",
+    no: "",
+  },
+  SDG11_b: {
+    en: "11.b Disaster plans",
+    no: "",
+  },
+  SDG11_c: {
+    en: "11.c Construction (developing countries)",
+    no: "",
+  },
+  SDG13_01: {
+    en: "13.1 Disasters",
+    no: "",
+  },
+  SDG13_02: {
+    en: "13.2 National plans",
+    no: "",
+  },
+  SDG13_03: {
+    en: "13.3 Capacity",
+    no: "",
+  },
+  SDG13_0a: {
+    en: "13.a Climate funding",
+    no: "",
+  },
+  SDG13_b: {
+    en: "13.b Capacity (developing countries)",
+    no: "",
+  },
+  SDG14_01: {
+    en: "14.1 Pollution",
+    no: "",
+  },
+  SDG14_02: {
+    en: "14.2 Conservation and management",
+    no: "",
+  },
+  SDG14_03: {
+    en: "14.3 Ocean acidification",
+    no: "",
+  },
+  SDG14_04: {
+    en: "14.4 Sustinable fishing",
+    no: "",
+  },
+  SDG14_05: {
+    en: "14.5 Protected areas",
+    no: "",
+  },
+  SDG14_06: {
+    en: "14.6 Fisheries subsidies",
+    no: "",
+  },
+  SDG14_07: {
+    en: "14.7 Benefits (SIDS/LEDC)",
+    no: "",
+  },
+  SDG14_0a: {
+    en: "14.a Infrastructure and benefits",
+    no: "",
+  },
+  SDG14_b: {
+    en: "14.b Small-scale fishers",
+    no: "",
+  },
+  SDG14_c: {
+    en: "14.c Laws and conventions",
+    no: "",
+  },
+  SDG15_01: {
+    en: "15.1 Conservation and use",
+    no: "",
+  },
+  SDG15_02: {
+    en: "15.2 Forests",
+    no: "",
+  },
+  SDG15_03: {
+    en: "15.1 Land degredation",
+    no: "",
+  },
+  SDG15_04: {
+    en: "15.4 Mountains",
+    no: "",
+  },
+  SDG15_05: {
+    en: "15.5 Habitats, diversity and extinction",
+    no: "",
+  },
+  SDG15_06: {
+    en: "15.6 Sharing genetic resources",
+    no: "",
+  },
+  SDG15_07: {
+    en: "15.7 Poaching and demand",
+    no: "",
+  },
+  SDG15_08: {
+    en: "15.8 Invasive species",
+    no: "",
+  },
+  SDG15_09: {
+    en: "15.9 Biodiversity planning",
+    no: "",
+  },
+  SDG15_0a: {
+    en: "15.a Financial resources",
+    no: "",
+  },
+  SDG15_b: {
+    en: "15.b Funding for forest management",
+    no: "",
+  },
+  SDG15_c: {
+    en: "15.c Poaching and livlihoods",
     no: "",
   },
 }
