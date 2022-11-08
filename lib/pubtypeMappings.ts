688181ae5835dad90421598651ac9ce1a04b80f9
Caroline Armitage <42732381+Caroline-A@users.noreply.github.com>
2022-11-08 16:47:15 +0100

    First draft

---
+++ lib/pubtypeMappings.ts
@@ -0,0 +1,173 @@
+export const categoryMapping = {
+  Tidsskriftspublikasjon: {
+    en: "Journal article",
+    no: "Tidsskriftspublikasjon",
+  },
+  Del av bok/rapport: {
+    en: "Chapter in book/report",
+    no: "Del av bok/rapport",
+  },
+  Rapport/avhandling: {
+    en: "Report/dissertation/thesis",
+    no: "Rapport/avhandling",
+  },
+  Bok: {
+    en: "Book",
+    no: "Bok",
+  }
+}
+
+export const subcategoryMapping = {
+  Anmeldelse: {
+    en: "Book review",
+    no: "Anmeldelse",
+  },
+  Annet: {
+    en: "Other",
+    no: "Annet",
+  },
+  Brev til redaktøren: {
+    en: "Letter to the editor",
+    no: "Brev til redaktøren",
+  },
+  Doktorgradsavhandling: {
+    en: "Dissertation (PhD)",
+    no: "Doktorgradsavhandling",
+  }
+  Errata: {
+    en: "Errata",
+    no: "Errata",
+  },
+  Fagartikkel: {
+    en: "Professional/Trade article",
+    no: "Fagartikkel",
+  },
+  Fagbok: {
+    en: "Professional/Trade book",
+    no: "Fagbok",
+  },
+  Faglig kapittel: {
+    en: "Professional/Trade chapter",
+    no: "Faglig kapittel",
+  },
+  Forord: {
+    en: "Forword",
+    no: "Forord",
+  },
+  Forskerlinjeoppgave: {
+    en: "Undergraduate Research assignment",
+    no: "Forskerlinjeoppgave",
+  },
+  Hovedfagsoppgave: {
+    en: "Hovedfagsoppgave",
+    no: "Hovedfagsoppgave",
+  },
+  Innledning: {
+    en: "Introduction",
+    no: "Innledning",
+  },
+  Intervju tidsskrift: {
+    en: "Interview",
+    no: "Intervju",
+  },
+  Kompendium: {
+    en: "Compendium",
+    no: "Kompendium",
+  },
+  Kronikk: {
+    en: "Opinion/popular article",
+    no: "Kronikk",
+  },
+  Leder: {
+    en: "Editorial",
+    no: "Leder",
+  },
+  Leksikon: {
+    en: "Dictionary",
+    no: "Leksikon",
+  },
+  Leksikonartikkel: {
+    en: "Dictionary article",
+    no: "Leksikonartikkel",
+  },
+  Leserinnlegg: {
+    en: "Reader's letter",
+    no: "Leserinnlegg",
+  },
+  Lærebok: {
+    en: "Textbook",
+    no: "Lærebok",
+  },
+  Magisteravhandling: {
+    en: "Magisteravhandling",
+    no: "Magisteravhandling",
+  },
+  Mastergradsoppgave: {
+    en: "Thesis (Master)",
+    no: "Mastergradsoppgave",
+  },
+  Oppslagsverk: {
+    en: "Encyclopedia",
+    no: "Oppslagsverk",
+  },
+  Lærebok: {
+    en: "Textbook",
+    no: "Lærebok",
+  },
+  Populærvitenskapelig artikkel: {
+    en: "Popular science article",
+    no: "Populærvitenskapelig artikkel",
+  },
+  Populærvitenskapelig bok: {
+    en: "Popular science book",
+    no: "Populærvitenskapelig bok",
+  },
+  Populærvitenskapelig kapittel/artikkel: {
+    en: "Popular science chapter",
+    no: "Populærvitenskapelig kapittel/artikkel",
+  },
+  Rapport: {
+    en: "Report",
+    no: "Rapport",
+  },
+  Sammendrag/abstract: {
+    en: "Summary",
+    no: "Sammendrag/abstract",
+  },
+  Short communication: {
+    en: "Short communication",
+    no: "Short communication",
+  },
+  Utstillingskatalog: {
+    en: "Exhibition catalogue",
+    no: "Utstillingskatalog",
+  },
+  Vitenskapelig Kapittel/Artikkel/Konferanseartikkel: {
+    en: "Scientific chapter or conference proceedings",
+    no: "Vitenskapelig Kapittel/Artikkel/Konferanseartikkel",
+  },
+  Vitenskapelig antologi/Konferanseserie: {
+    en: "Scientific anthology",
+    no: "Vitenskapelig antologi/Konferanseserie",
+  },
+  Vitenskapelig artikkel: {
+    en: "Scientific article",
+    no: "Vitenskapelig artikkel'",
+  },
+  Vitenskapelig kommentarutgave: {
+    en: "Scientific textual commentary",
+    no: "Vitenskapelig kommentarutgave",
+  },
+  Vitenskapelig monografi: {
+    en: "Scientific book",
+    no: "Vitenskapelig monografi",
+  },
+  Vitenskapelig oversiktsartikkel/review: {
+    en: "Scientific review article",
+    no: "Vitenskapelig oversiktsartikkel",
+  },
+  Lisensiatavhandling: {
+    en: "Lisensiatavhandling",
+    no: "Lisensiatavhandling",
+  }
+}
