21bc90564d84283fb9859e18242d732e46311641
Caroline Armitage <42732381+Caroline-A@users.noreply.github.com>
2022-11-15 16:31:54 +0100

    Create mentionsmapping.ts

---
+++ lib/mentionsmapping.ts
@@ -0,0 +1,11 @@
+export const mentionsMapping = {
+  'mentionsNorway': {
+    en: "Mentions Norway",
+    no: "Nevner Norge",
+  },
+  'mentionsSDG': {
+    en: "Mentions the SDGs",
+    no: "Nevner bærekraftsmålene",
+  }
+}
+
