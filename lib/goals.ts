e1c9d2a1d09a862d7776ecc3c6b8f61602dfd71e
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2022-11-29 11:13:50 +0100

    fix goals links and remove footer link to NB

--- lib/goals.ts
+++ lib/goals.ts
@@ -3,102 +3,102 @@ export const goals = [
     label: 'SDG-1',
     disabled: false,
     image: 'images/sdg/E-WEB-Goal-01.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG01&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG01&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-2',
     disabled: false,
     image: 'images/sdg/E-WEB-Goal-02.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG02&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG02&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-3',
     disabled: false,
     image: 'images/sdg/E-WEB-Goal-03.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG03&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG03&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-4',
     disabled: false,
     image: 'images/sdg/E-WEB-Goal-04.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG04&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG04&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-5',
     disabled: true,
     image: 'images/sdg/E-WEB-Goal-05.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG05&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG05&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-6',
     disabled: true,
     image: 'images/sdg/E-WEB-Goal-06.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG06&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG06&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-7',
     disabled: false,
     image: 'images/sdg/E-WEB-Goal-07.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG07&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG07&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-8',
     disabled: true,
     image: 'images/sdg/E-WEB-Goal-08.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG08&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG08&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-9',
     disabled: true,
     image: 'images/sdg/E-WEB-Goal-09.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG09&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG09&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-10',
     disabled: true,
     image: 'images/sdg/E-WEB-Goal-10.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG10&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG10&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-11',
     disabled: false,
     image: 'images/sdg/E-WEB-Goal-11.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG11&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG11&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-12',
     disabled: true,
     image: 'images/sdg/E-WEB-Goal-12.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG12&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG12&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-13',
     disabled: false,
     image: 'images/sdg/E-WEB-Goal-13.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG13&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG13&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-14',
     disabled: false,
     image: 'images/sdg/E-WEB-Goal-14.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG14&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG14&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-15',
     disabled: false,
     image: 'images/sdg/E-WEB-Goal-15.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG15&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG15&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-16',
     disabled: true,
     image: 'images/sdg/E-WEB-Goal-16.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG16&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG16&filters%5B0%5D%5Btype%5D=any'
   },
   {
     label: 'SDG-17',
     disabled: true,
     image: 'images/sdg/E-WEB-Goal-17.png',
-    link: 'search?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG17&filters%5B0%5D%5Btype%5D=any'
+    link: 'sok?size=n_20_n&filters%5B0%5D%5Bfield%5D=SDG_topic.keyword&filters%5B0%5D%5Bvalues%5D%5B0%5D=SDG17&filters%5B0%5D%5Btype%5D=any'
   }
 ]
