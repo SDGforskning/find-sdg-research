c008f152c345cc2aba44b5fe9db9ce193e59a8e3
Tarje Lavik <tarje.lavik@gmail.com>
2023-05-25 13:18:36 +0200

    move font to _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,12 +9,6 @@ export default function Nextra({
 }: AppProps): ReactElement {
   return (
     <>
-      <Head>
-        <link
-          href='https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap'
-          rel='stylesheet'
-        />
-      </Head>
       <Component {...pageProps} />
     </>
   )
