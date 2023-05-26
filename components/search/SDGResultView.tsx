53a9351da480e85aa7ea2b355b8681617a1da17a
Tarje Lavik <tarje.lavik@gmail.com>
2023-05-26 11:45:12 +0200

    fix low contrast

--- components/search/SDGResultView.tsx
+++ components/search/SDGResultView.tsx
@@ -173,7 +173,7 @@ const CustomResultView = ({
 
         <div className='flex md:flex-1 gap-3 md:gap-2 md:pl-5 md:w-5/12 w-full flex-wrap'>
           {data[locale].fulltextlink?.raw !== 'No open link found' ? (
-            <div className='px-3 py-2 text-white font-bold bg-green-500 rounded'>
+            <div className='px-3 py-2 text-white font-bold bg-green-700 rounded'>
               <a href={data[locale].fulltextlink.raw} target="_blank" rel="nonreferrer">
                 <LockOpenIcon className='inline w-4 h-4 -mt-1' /> {locale === 'en' ? 'Read the publication' : 'Les publikasjonen'} <ArrowUpRightIcon className="inline h-4 w-4 text-white" />
               </a>
