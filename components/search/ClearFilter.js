6c8e7637be3db0d27ff923d07ef9fd3697412d35
Tarje Lavik <tarje.lavik@gmail.com>
2023-06-02 15:15:47 +0200

    fix: :bug: Fix search uu issues
    Fixes #95

--- components/search/ClearFilter.js
+++ components/search/ClearFilter.js
@@ -1,10 +1,18 @@
 import { withSearch } from "@elastic/react-search-ui";
+import { useRouter } from 'next/router';
 
 function ClearFilters({ filters, clearFilters }) {
+  const locale = useRouter();
   return (
-    <div className='mb-3'>
-      <button onClick={() => clearFilters()}>
-        Clear {filters.length} Filters
+    <div className='my-3'>
+      <button
+        onClick={() => clearFilters()}
+        className='text-sm inline-flex items-center leading-sm bg-slate-200 dark:bg-slate-800 px-2 lg:py-1 rounded'
+      >
+        {locale === 'en'
+          ? `Clear ${filters.length} filter(s)`
+          : `Fjern ${filters.length} filter`
+        }
       </button>
     </div>
   );
