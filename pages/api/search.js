e804a037cda6159de9d1a2a62aa6d52a15422eb4
Tarje Lavik <tarje.lavik@gmail.com>
2023-06-14 09:49:41 +0200

    use the lastest test index

--- pages/api/search.js
+++ pages/api/search.js
@@ -2,7 +2,7 @@ import ElasticsearchAPIConnector from "@elastic/search-ui-elasticsearch-connecto
 
 const connector = new ElasticsearchAPIConnector({
   host: process.env.ES_HOST,
-  index: 'sdg-2023-05-10',
+  index: 'sdg-20230614073359',
   apiKey: process.env.ES_APIKEY,
 });
 
