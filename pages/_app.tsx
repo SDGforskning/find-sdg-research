995de051a2e11c245035ba20910166c01b0242ef
Tarje Lavik <tarje.lavik@gmail.com>
2023-06-27 11:28:49 +0200

    test matomo-next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,27 @@
-import type { ReactElement } from 'react'
+import { useRef, type ReactElement, useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import "../style.css"
-import Script from 'next/script'
+import { init } from "@socialgouv/matomo-next";
+
+const MATOMO_URL = 'https://stats.uib.no/';
+const MATOMO_SITE_ID = '82';
 
 export default function Nextra({
   Component,
   pageProps
 }: AppProps): ReactElement {
+  const matomoInitialized = useRef(false)
+
+  useEffect(() => {
+    if (MATOMO_URL && MATOMO_SITE_ID && matomoInitialized.current === false) {
+      init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID })
+    }
+    return () => {
+      matomoInitialized.current = true
+    }
+  }, [])
   return (
     <>
-      <Script strategy="afterInteractive" id="matomoAnalytics">
-        {`
-      var _paq = window._paq = window._paq || [];
-      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-      _paq.push(['trackPageView']);
-      _paq.push(['enableLinkTracking']);
-      (function() {
-        var u="//stats.uib.no/";
-        _paq.push(['setTrackerUrl', u+'matomo.php']);
-        _paq.push(['setSiteId', '82']);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-      })();
-		`}
-      </Script>
       <Component {...pageProps} />
     </>
   )
