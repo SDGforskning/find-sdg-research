9527d04af7409dda32aeb31e668315cd78267975
Caroline Armitage <42732381+Caroline-A@users.noreply.github.com>
2023-07-04 14:15:32 +0200

    test remove scrollbars?

--- components/tableau/index.tsx
+++ components/tableau/index.tsx
@@ -10,7 +10,7 @@ const TableauNoSSR = dynamic(() => import('@stoddabr/react-tableau-embed-live').
 export const Tableau = (
   { hostUrl, height, width = '100%', toolbar = 'hidden', device = 'default' }: { hostUrl: string, height?: number | string, width?: number | string, toolbar?: 'top' | 'bottom' | 'hidden', device?: 'default' | 'desktop' | 'tablet' | 'phone' }) => {
   return (
-    <div className={`mx-auto w-full bg-slate-100 rounded p-1 my-3 overflow-scroll`}>
+    <div className={`mx-auto w-full bg-slate-100 rounded p-1 my-3 overflow-auto`}>
       <TableauNoSSR
         sourceUrl={hostUrl}
         height={height}
