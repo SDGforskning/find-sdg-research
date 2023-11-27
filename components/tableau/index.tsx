ca23fc92a49bf323ad6eb5a17ef6261d4a0698bf
Tarje Lavik <tarje.lavik@gmail.com>
2023-11-27 20:51:44 +0100

    fix import issue

--- components/tableau/index.tsx
+++ components/tableau/index.tsx
@@ -1,7 +1,7 @@
 'use client'
-import dynamic from 'next/dynamic';
+import { TableauEmbed } from "@stoddabr/react-tableau-embed-live";
+import { Suspense } from 'react';
 
-const TableauNoSSR = dynamic(() => import('@stoddabr/react-tableau-embed-live').then((module) => module.TableauEmbed), { ssr: false });
 /**
  * Tableau component that takes in a hostUrl and renders a Tableau widget
  * @param {*} param0 
@@ -11,7 +11,7 @@ export const Tableau = (
   { hostUrl, height, width = '100%', toolbar = 'hidden', device = 'default' }: { hostUrl: string, height?: number | string, width?: number | string, toolbar?: 'top' | 'bottom' | 'hidden', device?: 'default' | 'desktop' | 'tablet' | 'phone' }) => {
   return (
     <div className={`mx-auto w-full bg-slate-100 rounded p-1 my-3 overflow-auto`}>
-      <TableauNoSSR
+      <TableauEmbed
         sourceUrl={hostUrl}
         height={height}
         width={width}
