748d9745bc0b0924a27ff13e3c9773d38154afde
Tarje Lavik <tarje.lavik@gmail.com>
2023-12-05 13:36:50 +0100

    use default middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,1 @@
-import { NextRequest } from 'next/server';
-import { locales } from 'nextra/locales';
-
-export async function middleware(req: NextRequest) {
-  const pathIsStaticFile = req.nextUrl.pathname.startsWith('/static/');
-  if (pathIsStaticFile) {
-    return;
-  }
-  return locales(req);
-}
+export { locales as middleware } from 'nextra/locales'
