4e9fab16624dbd7c9f40b4e24db43c8d0df54ccf
Tarje Lavik <tarje.lavik@gmail.com>
2023-12-05 11:44:47 +0100

    remove unstable config

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,6 @@ import nextra from 'nextra'
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  unstable_staticImage: true,
-  unstable_flexsearch: {
-    codeblocks: true
-  },
-  unstable_defaultShowCopyCode: true
 })
 
 export default withNextra({
